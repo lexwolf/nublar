f68005704b732a455cd65577bc7f0faf038abaf0
lexwolf <alessandro.veltri@gmail.com>
2026-03-13 12:22:39 +0100

    Local IntelliSense compatibility improved

---
+++ .vscode/cup_intellisense_qs.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef CUP_BACKEND_QUASI_STATIC
+#define CUP_BACKEND_QUASI_STATIC
+#endif
+
+// Needed by cup/quasi_static/frohlich.hpp when parsed standalone.
+#include <nano_geo_matrix/quasi_static/geometry/single.hpp>
+
+#include <cup.hpp>
