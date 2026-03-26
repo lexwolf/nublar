d4770d3798c8560e03deeca77f44073c8a0a07d1
lexwolf <alessandro.veltri@gmail.com>
2026-03-26 10:12:31 +0100

    chore: stop tracking build artifacts (build/ directory)

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alessandro/GitHub/Academia/nublar/extern/nano_geo_matrix
-# Build directory: /home/alessandro/GitHub/Academia/nublar/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("tests")
